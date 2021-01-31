933790c5545b895bd138a0146ea2aa34845f4ca9
root <root@rh7pc.local>
2021-01-31 11:36:22 +0000

    AK first edit

--- ansible_checks.sh
+++ ansible_checks.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+# Amit Karia www.it-howto.co.uk
 ansible appservers -m shell -a 'netstat -apn | grep ^tcp'
 ansible dbservers -m shell -a 'netstat -apn | grep ^tcp'
 ansible webservers -m shell -a 'netstat -apn | grep ^tcp'
