ff08b3bfc790c94b75036bd9dd5c86b6e06d6224
root <root@lab2.local>
2021-03-20 17:29:41 +0000

    Amit Karia March 20 2021

---
+++ initial_setup.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+yum install epel-release -y
+yum install dialog -y
+yum install emacs-nox -y
+yum install net-tools -y
+yum install net-snmp -y
+yum install net-snmp-utils -y
+yum install ansible -y
