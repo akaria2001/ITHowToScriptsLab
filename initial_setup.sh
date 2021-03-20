78e8ada604eca8809da580df46c0cfa79777d222
akaria2001 <51513278+akaria2001@users.noreply.github.com>
2021-03-20 17:50:54 +0000

    Update initial_setup.sh
    Update

--- initial_setup.sh
+++ initial_setup.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
+yum install make -y
+yum install gcc -y
+yum install kernel-headers -y
+yum install "kernel-devel-uname-r == $(uname -r)" -y
 yum install epel-release -y
 yum install dialog -y
 yum install emacs-nox -y
